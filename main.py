0830ffea783b7c001e7174d42e8f901618ce9b13
berkaldar <berk_kaldar@yahoo.com.tr>
2022-11-16 17:33:08 +0300

    Testing for Etiya Academy

---
+++ main.py
@@ -0,0 +1,59 @@
+print("Merhaba Etiya")
+
+
+#  string = metinsel ifade
+text = "10"
+print(text)
+
+# integer = tam sayı
+number = 10
+print(number)
+
+# double,float,decimal = ondalıklı sayı
+dnumber = 5.3
+print(dnumber)
+
+#! matematiksel operatörler
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 3) #! bölümünden kalan sonuç
+#! matematiksel operatörler
+
+#! boolean,bool = true veya false
+isVerified = True
+
+#! mantıksal, karşılaştırma operatörler => her mantıksal operatör boolean değer döner
+print (1 == 2)
+print (2 != 2)
+
+print(2 > 2)
+print(2 < 2)
+#! mantıksal operatörler
+
+print (2 >= 2)
+print (2 <= 2)
+
+
+
+print(10%2==0)
+print(50/2 == 25.0)
+
+#! mantıksal operatörler
+
+#! string operasyonları
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("Mer"))
+print(text.endswith("Etiya"))
+
+name = "Berk"
+age = "30"
+company = "Etiya"
+
+#! Berk 30 yaşında Kodlamaio'da çalışıyor
+#print(name + " " + age + " yaşında " + company + "'de çalışıyor")
+print(f"{name} {age} yaşında {company}'de çalışıyor")
+#!
